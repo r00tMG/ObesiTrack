91521ec02dda0e17685f79b7b5d92682aac5d794
r00tMG <meissagningue7@gmail.com>
2025-09-12 21:57:16 +0100

    update configuration sqlite

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 #DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-DATABASE_URL = os.getenv("DB_URL")
+DATABASE_URL = os.getenv("DB_URL", "sqlite:///./data/obesitrack_db.sqlite3")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
